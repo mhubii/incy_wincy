7344bfeb983bd974e2eb3333c4bd2eb93759b35a
martin <m.huber_1994@hotmail.de>
2019-02-20 02:31:45 +0100

    finished automatic plots

--- eval/plot.py
+++ eval/plot.py
@@ -4,18 +4,17 @@ import matplotlib.pyplot as plt
 # generate plots
 def plot(t_ii, t_ei, a_ii, a_ei):
     # determine metrics
-    #mse = np.linalg.norm(a_ii - a_ei)
+    mse = np.linalg.norm(a_ii - a_ei[:-1,:])
 
-    x_ii = a_ii[:,0]
+    y_ii = a_ii[:,1]
     z_ii = a_ii[:,2]
 
-    x_ei = a_ei[:,0]
+    y_ei = a_ei[:,1]
     z_ei = a_ei[:,2]
 
-    plt.plot(x_ii, z_ii, label="explicit, time {}s".format(t_ii[1]))
-    plt.plot(x_ei, z_ei, "--", label="implicit, time {}s".format(t_ei[1]))
-    plt.legend()
-    plt.show()
+    plt.plot(y_ii, z_ii, label="exp, t {}s, v {}m/s, mse {:.2f}m".format(t_ii[1], t_ii[0], mse))
+    plt.plot(y_ei, z_ei, "--", label="imp, t {}s, v {}m/s".format(t_ei[1], t_ei[0]))
+    #plt.show()
 
 
 
@@ -28,10 +27,14 @@ if __name__ == "__main__":
     animation_ii = []
 
     for v in time_ii:
-        animation_ii.append(np.genfromtxt("out/animation_ii_vinit_{}.csv".format(str(int(v[0]))), delimiter=","))
-        animation_ei.append(np.genfromtxt("out/animation_ei_vinit_{}.csv".format(str(int(v[0]))), delimiter=","))
+        animation_ii.append(np.genfromtxt("out/animation_ii_vinit_{}.csv".format(int(v[0])), delimiter=","))
+        animation_ei.append(np.genfromtxt("out/animation_ei_vinit_{}.csv".format(int(v[0])), delimiter=","))
 
     # plot results
     for i in range(time_ii.shape[0]):
         plot(time_ii[i], time_ei[i], animation_ii[i], animation_ei[i])
-
+        plt.title("Bouncing Ball Benchmark with Explicit and Implicit Integrators")
+        plt.xlabel("y/m")
+        plt.ylabel("z/m")
+        plt.legend()
+        plt.savefig("compare.pdf")
--- eval/run_integrators.py
+++ eval/run_integrators.py
@@ -1,7 +1,7 @@
 import subprocess
 import numpy as np
 
-vel = np.arange(0, 5)
+vel = np.arange(0, 3)
 
 # run processes
 subprocess.run(["ls", "-l"])
@@ -9,9 +9,9 @@ subprocess.run(["ls", "-l"])
 for v in vel:
     subprocess.run(["../build/bouncingBallBenchmark", 
         "-m", "../models/ballPlaneContact.lua",
-        "-v", str(v),
+        "-v", str(int(v)),
         "-o", "out/"])
     subprocess.run(["../src/numerical_jacobian/build/numericalJacobian",
         "-m", "../models/ballPlaneContact.lua",
-        "-v", str(v),
+        "-v", str(int(v)),
         "-o", "out/"])
