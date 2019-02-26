1cbe8611c76bb3e0ccec714b49edbd0d5e4c4b95
martin <m.huber_1994@hotmail.de>
2019-02-26 16:35:57 +0100

    added ppo and rollouts

---
+++ src/proximal_policy_optimization/ProximalPolicyOptimization.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <torch/torch.h>
+
+class ProximalPolicyOptimization
+{
+private:
+    /* data */
+public:
+    ProximalPolicyOptimization(/* args */);
+    ~ProximalPolicyOptimization();
+    auto update() -> void;
+};
+
+ProximalPolicyOptimization::ProximalPolicyOptimization(/* args */)
+{
+}
+
+ProximalPolicyOptimization::~ProximalPolicyOptimization()
+{
+}
+
+auto ProximalPolicyOptimization::update() -> void
+{
+
+}
