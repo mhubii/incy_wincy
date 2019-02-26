898a3a879ce9c56aa2a7c2c52d848549dc355c8b
martin <m.huber_1994@hotmail.de>
2019-02-26 16:35:34 +0100

    added non parametrized to model

--- src/proximal_policy_optimization/Models.h
+++ src/proximal_policy_optimization/Models.h
@@ -1,23 +1,26 @@
 #pragma once
 
 #include <torch/torch.h>
+#include <math.h>
 
 // Network model for Proximal Policy Optimization on Incy Wincy.
-struct ActorCritic : public torch::nn::Module {
+struct ActorCritic : public torch::nn::Module 
+{
 
     ActorCritic(int64_t n_in, int64_t n_out, float std)
         : // Actor.
           a_lin1_(torch::nn::Linear(n_in, 16)),
           a_lin2_(torch::nn::Linear(16, 32)),
           a_lin3_(torch::nn::Linear(32, n_out)),
+          mu_(torch::full(n_out, 0.)),
           std_(torch::full(n_out, std)),
           
           // Critic
           c_lin1_(torch::nn::Linear(n_in, 16)),
           c_lin2_(torch::nn::Linear(16, 32)),
           c_lin3_(torch::nn::Linear(32, n_out)),
-          c_val_(torch::nn::Linear(n_out, 1)) {
-
+          c_val_(torch::nn::Linear(n_out, 1)) 
+    {
         // Register the modules.
         register_module("a_lin1", a_lin1_);
         register_module("a_lin2", a_lin2_);
@@ -31,46 +34,93 @@ struct ActorCritic : public torch::nn::Module {
     }
 
     // Forward pass.
-    auto forward(torch::Tensor x, torch::Device device) -> std::tuple<torch::Tensor, torch::Tensor> {
-
+    auto forward(torch::Tensor x, torch::Device device) -> std::tuple<torch::Tensor, torch::Tensor> 
+    {
+ 
         // Actor.
-        torch::Tensor mu = torch::tanh(a_lin1_->forward(x));
-        mu = torch::tanh(a_lin2_->forward(mu));
-        mu = torch::tanh(a_lin3_->forward(mu));
+        mu_ = torch::relu(a_lin1_->forward(x));
+        mu_ = torch::relu(a_lin2_->forward(mu_));
+        mu_ = a_lin3_->forward(mu_);
 
         // Critic.
-        torch::Tensor val = torch::tanh(c_lin1_(x));
-        val = torch::tanh(c_lin2_(val));
-        val = torch::tanh(c_lin3_(val));
+        torch::Tensor val = torch::relu(c_lin1_(x));
+        val = torch::relu(c_lin2_(val));
+        val = c_lin3_(val);
         val = c_val_(val);
 
         // Reparametrization trick.
-        if (this->is_training()) {
-            torch::Tensor sample = torch::normal(torch::zeros(mu.sizes(), device), torch::ones(mu.sizes(), device)).detach();
-            sample = sample.mul(std_).add(mu).expand_as(mu);
-            return std::make_tuple(sample, val);  
+        if (this->is_training()) 
+        {
+            torch::NoGradGuard no_grad;
+
+            torch::Tensor action = torch::normal(mu_, std_.expand_as(mu_)).to(device);
+            return std::make_tuple(action, val);  
         }
-        else {
-            return std::make_tuple(mu, val);  
+        else 
+        {
+            return std::make_tuple(mu_, val);  
+        }
+    }
+
+    auto entropy() -> torch::Tensor
+    {
+        // Differential entropy of normal distribution. For reference https://pytorch.org/docs/stable/_modules/torch/distributions/normal.html#Normal
+        return 0.5 + 0.5*log(2*M_PI) + std_.log();
+    }
+
+    auto log_prob(torch::Tensor action) -> torch::Tensor
+    {
+        // Logarithmic probability of taken action, given the current distribution.
+        torch::Tensor var = std_*std_;
+        torch::Tensor log_scale = std_.log();
+
+        return -((action - mu_)*(action - mu_))/(2*var) - log_scale - log(sqrt(2*M_PI));
+    }
+
+    // Forward pass using reparametrization.
+    auto rforward(torch::Tensor x, torch::Device device) -> std::tuple<torch::Tensor, torch::Tensor> 
+    {
+ 
+        // Actor.
+        mu_ = torch::relu(a_lin1_->forward(x));
+        mu_ = torch::relu(a_lin2_->forward(mu_));
+        mu_ = a_lin3_->forward(mu_);
+
+        // Critic.
+        torch::Tensor val = torch::relu(c_lin1_(x));
+        val = torch::relu(c_lin2_(val));
+        val = c_lin3_(val);
+        val = c_val_(val);
+
+        // Reparametrization trick.
+        if (this->is_training()) 
+        {
+            torch::Tensor action = torch::normal(torch::zeros(mu_.sizes(), device), torch::ones(mu_.sizes(), device)).detach();
+            action = action.mul(std_).add(mu_).expand_as(mu_);
+            return std::make_tuple(action, val);  
+        }
+        else 
+        {
+            return std::make_tuple(mu_, val);  
         }
     }
 
     // Initialize network.
-    void init() {
-
+    void init() 
+    {
         torch::NoGradGuard no_grad;
 
-        for (auto& p: this->parameters()) {
+        for (auto& p: this->parameters()) 
+        {
             p.normal_(0.,0.1);
         }         
     }
 
     // Actor.
     torch::nn::Linear a_lin1_, a_lin2_, a_lin3_;
-
+    torch::Tensor mu_;
     torch::Tensor std_;
 
     // Critic.
     torch::nn::Linear c_lin1_, c_lin2_, c_lin3_, c_val_;
-    
 };
